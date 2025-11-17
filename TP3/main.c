c9cf3513c4b81d09ec638dc469b0d0bf7e52116d
jedd <jed@jed.jed>
2025-11-17 14:01:03 +0100

    suite pointe haut

--- TP3/main.c
+++ TP3/main.c
@@ -3,23 +3,36 @@
 #include <time.h>
 #include <unistd.h>
 
-void afficherPointeHaut(int LONGUEUR, int SPACENUMBER){
+int afficherPointeHaut(int LONGUEUR){
+    int last_line;
+    int spacenumber = (LONGUEUR-1)*3;
     for(int i=0; i < LONGUEUR; i++){
-        for(int j=0; j < SPACENUMBER-i; j++){
-            printf(" ");
+        if(i==LONGUEUR-1){
+            for(int j=0; j < spacenumber-i; j++){
+                printf("_");
+            }
+        }else{
+            for(int j=0; j < spacenumber-i; j++){
+                printf(" ");
+            }
         }
         printf("A");
         for(int j=0; j < i*2; j++){
             printf("A");
         }
+        if(i==LONGUEUR-1){
+            for(int j=0; j < spacenumber-i; j++){
+                printf("_");
+            }
+            last_line = spacenumber*2+1;
+        }
         printf("\n");
     }
+    return last_line;
 }
 
 int main(){
-    int LONGUEUR = 5;
-    int SPACENUMBER = 10;
-
-    afficherPointeHaut(LONGUEUR, SPACENUMBER);
+    int LONGUEUR = 10;
+    printf("%d\n",afficherPointeHaut(LONGUEUR));
     
 }
