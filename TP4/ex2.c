09e60c46b0734709deb22bd7ace712248cbc1691
jedd <jed@jed.jed>
2025-11-24 14:58:48 +0100

    Jolis prints (espacés et tout)

--- TP4/ex2.c
+++ TP4/ex2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void afficher_menu(){
-    printf("=== BANQUE - DISTRIBUTEUR ===\n");
+    printf("\n=== BANQUE - DISTRIBUTEUR ===\n");
     printf("1 - Faire un retrait\n");
     printf("2 - Quitter\n");
 }
@@ -24,7 +24,7 @@ int saisir_montant(){
         if(montant_valide(montant)){
             break;
         }else{
-            printf("Le montant doit etre entre 5 euros et 1000 euros, divisible par 5.\n> ");
+            printf("Le montant doit etre entre 5 euros et 1000 euros, divisible par 5 : ");
             scanf("%i", &montant);
         }
     }
@@ -33,19 +33,19 @@ int saisir_montant(){
 
 void calcul_distribution(int montant){
     int billets50 = 0, billets20 = 0, billets10 = 0, billets5 = 0;
-    billets50 = montant % 50;
-    montant -= billets50 * 50;
+    billets50 = (int)((float)montant / 50.0f);
+    montant = montant % 50;
 
-    billets20 = montant % 20;
-    montant -= billets20 * 20;
+    billets20 = (int)((float)montant / 20.0f);
+    montant = montant % 20;
 
-    billets10 = montant % 10;
-    montant -= billets10 * 10;
+    billets10 = (int)((float)montant / 10.0f);
+    montant = montant % 10;
 
-    billets5 = montant % 5;
-    montant -= billets5 * 5;
+    billets5 = (int)((float)montant / 5.0f);
+    montant = montant % 5;
 
-    printf("Billets distribues :\n");
+    printf("\nBillets distribues :\n");
     if(billets50>0){
         printf("%d billets de 50\n", billets50);
     }
@@ -68,7 +68,7 @@ int main(){
         printf("Votre choix : ");
         scanf("%i", &choix);
         while(choix!=1 && choix!=2){
-            printf("Soit 1 soit 2\n> ");
+            printf("Soit 1 soit 2 : ");
             scanf("%i", &choix);
         }
         switch (choix)
