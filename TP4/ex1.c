472a2e3c69b61f44ee33134192a2de789042fcab
jedd <jed@jed.jed>
2025-11-24 13:15:47 +0100

    etape 1

--- TP4/ex1.c
+++ TP4/ex1.c
@@ -2,7 +2,14 @@
 #include <stdlib.h>
 
 void afficherMenu(){
-    
+    printf("===== GESTION DE VOTES =====\n");
+    printf("1. Saisir le nombre d'eleves\n");
+    printf("2. Saisir les notes des eleves\n");
+    printf("3. Afficher toutes les notes\n");
+    printf("4. Afficher la moyenne d'un eleve\n");
+    printf("5. Afficher la moyenne generale\n");
+    printf("6. Afficher la meilleure note de chaque controle\n");
+    printf("0. Quitter\n");
 }
 
 int main(){
