1dc72308ef6fb9bfcee18b7d089c83c0b90cf556
jedd <jed@jed.jed>
2025-11-24 13:17:08 +0100

    etape 3

--- TP4/ex1.c
+++ TP4/ex1.c
@@ -19,6 +19,18 @@ int lireChoix(){
     return choix;
 }
 
+int saisirNombreEleves(){ // les commits de l'étape 1,2 et 3 sont presque tous faits au meme temps parce que j'ai oublié de commit et je viens de faire beaucoup de ctrl-z et ctrl-y pour pouvoir commit les etapes dans l'ordre
+    int nombre_saisi = 0;
+    while(nombre_saisi > 30 || nombre_saisi < 1){
+        printf("Entrez le nombre d'élèves (1 a 30) : ");
+        scanf("%d", &nombre_saisi);
+        if(nombre_saisi <= 30 || nombre_saisi >= 1){
+            return nombre_saisi;
+        }
+        printf("Valeur invalide\n");
+    }
+}
+
 int main(){
     
 }
